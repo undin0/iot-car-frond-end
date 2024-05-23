8db55870a3ca014b4fd340f491056544e4e90f52
undin0 <118146230+undin0@users.noreply.github.com>
2024-05-23 11:17:57 -0600

    Update main.js

--- control/web/js/main.js
+++ control/web/js/main.js
@@ -12,7 +12,7 @@ function callApi(estatus) {
   // Hacer una petición para un usuario con ID especifico
   axios
     .get(
-      "https://13.59.8.166/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" +
+      "https://3.145.139.71/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" +
         estatus
     )
     .then(function (response) {
